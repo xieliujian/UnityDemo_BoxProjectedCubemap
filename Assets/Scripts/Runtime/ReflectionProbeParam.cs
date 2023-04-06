7885822d6a161b420cffdb2ccfd9e2450aa065f8
xieliujian <377624289@qq.com>
2023-04-06 18:22:38 +0800

    1

--- Assets/Scripts/Runtime/ReflectionProbeParam.cs
+++ Assets/Scripts/Runtime/ReflectionProbeParam.cs
@@ -2,35 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-[ExecuteInEditMode]
-public class ReflectionProbeParam : MonoBehaviour
+namespace gtm.Scene
 {
-    ReflectionProbe m_ReflProbe;
-
-    [Header("Reflection Probe 中心点")]
-    public Vector3 reflProbeCenter;
-
-    [Header("Reflection Probe 碰撞Box的最小点")]
-    public Vector3 reflProbeBoxMin;
-
-    [Header("Reflection Probe 碰撞Box的最大点")]
-    public Vector3 reflProbeBoxMax;
-
-    // Start is called before the first frame update
-    void Start()
+    [ExecuteInEditMode]
+    public class ReflectionProbeParam : MonoBehaviour
     {
-        m_ReflProbe = GetComponent<ReflectionProbe>();
-    }
+        ReflectionProbe m_ReflProbe;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (m_ReflProbe != null)
+        [Header("Reflection Probe 中心点")]
+        public Vector3 reflProbeCenter;
+
+        [Header("Reflection Probe 碰撞Box的最小点")]
+        public Vector3 reflProbeBoxMin;
+
+        [Header("Reflection Probe 碰撞Box的最大点")]
+        public Vector3 reflProbeBoxMax;
+
+        // Start is called before the first frame update
+        void Start()
         {
-            reflProbeCenter = m_ReflProbe.bounds.center;
-            reflProbeBoxMin = m_ReflProbe.bounds.min;
-            reflProbeBoxMax = m_ReflProbe.bounds.max;
+            m_ReflProbe = GetComponent<ReflectionProbe>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (m_ReflProbe != null)
+            {
+                reflProbeCenter = m_ReflProbe.bounds.center;
+                reflProbeBoxMin = m_ReflProbe.bounds.min;
+                reflProbeBoxMax = m_ReflProbe.bounds.max;
+            }
         }
     }
 }
+
